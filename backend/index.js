9cc120055ff2df8e6e9c9beff83484584b8805c0
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-08 19:47:56 +0530

    mongodb connected and port created

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const main = require('./db');
+const express = require('express')
+
+main();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
