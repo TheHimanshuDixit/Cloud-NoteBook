8229acef9edbc3b75bec85d81ddc9f40fff77fc8
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-09 22:15:33 +0530

    JWT token and bycript for authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const User = require('../models/User');
-const { query, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+
+const JWT_SECRET = 'thisisasecret';
 
 // create a user using : POST "/api/auth/". Doesn't need to be Authenticated
-
 // @route   GET api/auth
-router.post('/createuser', [query('email', 'Enter a valid email').isEmail(), query('name', 'Enter a valid name').isLength({ min: 3 }), query('password', 'Enter a valid password').isLength({ min: 5 })], async (req, res) => {
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),  
+    body('password', 'Enter a valid password').isLength({ min: 5 })
+    ], async (req, res) => {
 
     // if there are errors then return bad request and the errors
     const result = validationResult(req);
@@ -21,15 +29,26 @@ router.post('/createuser', [query('email', 'Enter a valid email').isEmail(), que
             return res.status(400).json({ "Error": "User with this email already exists" });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         // create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
             .then(user => {
-                console.log(user);
-                res.json(user)
+                const data = {
+                    user: {
+                        id: user.id
+                    }
+                }
+                const authtoken = jwt.sign(data, JWT_SECRET) // create a token
+                console.log(authtoken);
+                res.json({ authtoken });
+                // console.log(user);
+                // res.json(user)
             })
             .catch(err => {
                 console.log(err);
