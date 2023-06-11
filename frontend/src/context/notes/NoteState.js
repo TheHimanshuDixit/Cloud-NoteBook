76a2d566943079663b290d4da84070ef517a5c56
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-12 00:23:53 +0530

    add notes and your notes frontend created

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import NoteContext from './noteContext'
-// import { useState } from 'react'
+import { useState } from 'react'
 
 const NoteState = (props) => {
     // const st = {
@@ -17,9 +17,33 @@ const NoteState = (props) => {
     //         })
     //     }, 1000);
     // }
+
+    const notesInitial = [
+        {
+            "_id": "6161b8b3c8b7a3b3e4c1b0a1",
+            "user": "615f9b3c8b7a3b3e4c1b0a1",
+            "title": "My title",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2021-10-09T17:00:19.000Z",
+            "__v": 0
+        },
+        {
+            "_id": "6161b8b3c8b7a3b3e4c1b0a2",
+            "user": "615f9b3c8b7a3b3e4c1b0a1",
+            "title": "My title",
+            "description": "My description",
+            "tag": "personal",
+            "date": "2021-10-09T17:00:19.000Z",
+            "__v": 0
+        }
+    ]
+
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
         // <NoteContext.Provider value={{state, update}}>
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
