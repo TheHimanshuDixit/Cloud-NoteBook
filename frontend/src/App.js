c2ae905cbbf7d1f9a0b9e9d8f80c9398acc74445
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-11 21:09:44 +0530

    components setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Cloud Notebook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
