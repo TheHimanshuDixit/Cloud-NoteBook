decbd7a60ba2cbeebe1dab9240736417520f9c1f
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-29 17:02:38 +0530

    shadow added in nav and foot

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = (props) => {
         <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-4 h-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
       </button>
 
-      <header className="text-gray-600 body-font">
+      <header className="text-gray-600 body-font shadow-lg">
         <div className="md:container md:mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link to={'/'} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
