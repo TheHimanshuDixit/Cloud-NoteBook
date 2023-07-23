6c49ffb5fbe0c9133b1225aefa604316e7f25b4a
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-23 17:14:51 +0530

    backend fixes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 // getting-started.js
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/cloudnotebook');
+  await mongoose.connect(MONGO_URI);
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-    console.log('connected to Mongo Successfully');
+  console.log('connected to Mongo Successfully');
 }
 
 module.exports = main;
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 main();
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 
